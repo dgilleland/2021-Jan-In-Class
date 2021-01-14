f17e72cc35422de75e67588c193c03fdd545c00c
Dan Gilleland <dgilleland@nait.ca>
2021-01-13 17:46:51 -0700

    Write to file as text and json

--- src/demos/CSharp/Simple File Input-Ouput/App/Program.cs
+++ src/demos/CSharp/Simple File Input-Ouput/App/Program.cs
@@ -2,6 +2,9 @@
 // 0) Using statements
 using System;
 using static System.Console;
+using System.IO; // This allows us access to the File and Directory and other classes
+using System.Text.Json; // This contains classes for working with JSON
+using System.Text.Json.Serialization; // contains classes/types for serializing (load/save) JSON
 
 // 1) Body of the Main method
 WriteLine("Hello World!");
@@ -14,13 +17,29 @@ int qty = int.Parse(ReadLine());
 Product item = new Product(name, price, qty);
 Display(item);
 
+const string fileName = "Inventory.txt"; // Where this file will exist??
+if (File.Exists(fileName)) // Add to the existing file
+    File.AppendAllText(fileName, $"\n{item}");
+else // Create a new file
+    File.WriteAllText(fileName, item.ToString());
+OutputToJSON(item);
+
 // 2) Local methods/properties/fields
+void OutputToJSON(object data) // This makes it more "general purpose" to output any kind of object
+{
+    const string fileName = "Inventory.json";
+    string jsonString = JsonSerializer.Serialize(data);
+    File.WriteAllText(fileName, jsonString);
+}
+
+
 void Display(object data)
 {
     WriteLine(data);
 }
 
-
-// 3) Other declaration types
+// 3) Other declaration types - recognizes that all the code above is inside the default Program class
 // .NET 5 introduces the record keyword that is used for a special kind of class
+// A record is a data type that can be defined by just indicating the signature of the constructor
+// where the names of the parameters also become the names of properties on the data type.
 public record Product(string Name, double Price, int QuantityOnHand);
