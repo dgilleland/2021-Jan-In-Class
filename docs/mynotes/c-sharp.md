5b728835fa8488516e43613b165c18ca536da53e
Dan Gilleland <dgilleland@nait.ca>
2021-01-14 08:55:18 -0700

    File I/O + link updates

--- docs/mynotes/c-sharp.md
+++ docs/mynotes/c-sharp.md
@@ -2,8 +2,8 @@
 
 ## Resources
 
-- [ ] [Simplified C# Grammar](https://programming-0101.github.io/TheBook/Teach/chapter1-7.html#flow-control-statements).
-- [ ] [What is a Computer Program?](https://programming-0101.github.io/TheBook/Teach/chapter1.html#introduction)
+- [ ] [Simplified C# Grammar](https://programming-0101.github.io/TheBook/Teach/chapter1-7.html).
+- [ ] [What is a Computer Program?](https://programming-0101.github.io/TheBook/Teach/chapter1.html)
 - [ ] [Foundational Concepts](https://programming-0101.github.io/slides/OOP-Ramp-Up/00-FoundationalConcepts.html)
 - [ ] [Simplified Grammar](https://programming-0101.github.io/slides/OOP-Ramp-Up/01-Grammar.html)
 - [ ] [Enumerations](https://programming-0101.github.io/slides/OOP-Ramp-Up/02-Enum.html#1)
@@ -11,3 +11,4 @@
 - [ ] [Inheritance and Composition](https://programming-0101.github.io/slides/OOP-Ramp-Up/04-Objects.html)
 - [ ] [Arrays](https://programming-0101.github.io/slides/OOP-Ramp-Up/05-Arrays.html#1)
 - [ ] TBA
+
--- src/demos/CSharp/Simple File Input-Output/App/Program.cs
+++ src/demos/CSharp/Simple File Input-Output/App/Program.cs
@@ -5,6 +5,9 @@
 // 0) Using statements for the libraries that you are working with
 using System;
 using static System.Console;
+using System.IO; // This allows us access to the File and Directory and other classes
+using System.Text.Json; // This contains classes for working with JSON
+using System.Text.Json.Serialization; // Contains classes/types/extensions for serializing JSON (load/save)
 
 // 1) Body of the Main method
 // WriteLine is a public static method of the Console class
@@ -16,7 +19,27 @@ decimal price = decimal.Parse(ReadLine());
 Product item = new Product(name, price);
 Display(item);
 
+const string fileName = "Inventory.txt"; // Where will this file exist??
+if (File.Exists(fileName)) // Add to the existing file
+    File.AppendAllText(fileName, $"\n{item}");
+else // Create a new file
+    // Open/create the "Inventory.txt" file and write out all of the text from the item object
+    File.WriteAllText(fileName, item.ToString());
+
+// Now, let's output this as JSON
+OutputToJSON(fileName, item);
+
 // 2) Declare other members of the class that is wrapping this Main method - like methods/properties
+void OutputToJSON(string filePath, Product data)
+{
+    if (filePath.EndsWith(".txt"))
+        filePath = filePath.Replace(".txt", ".json");
+    else if (!filePath.EndsWith(".json"))
+        filePath = filePath + ".json";
+    string jsonString = JsonSerializer.Serialize(data);
+    File.WriteAllText(filePath, jsonString);
+}
+
 void Display(object thing)
 {
     WriteLine(thing);
