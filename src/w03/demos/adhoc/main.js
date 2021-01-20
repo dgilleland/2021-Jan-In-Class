78d97c44143232dcb568b97a89ee2fe953e6a944
Dan Gilleland <dgilleland@nait.ca>
2021-01-20 10:27:38 -0700

    Ad-Hoc demo

---
+++ src/w03/demos/adhoc/main.js
@@ -0,0 +1,6 @@
+// Explore functions
+const add = function(first, second) {
+    console.log('The first parameter is ', first);
+    console.log('The second parameter is ', second);
+    return first + second;
+}
