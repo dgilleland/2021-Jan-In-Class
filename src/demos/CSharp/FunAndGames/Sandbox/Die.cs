a97c58439d8d60ebe6719dd08b948a26be95ef67
Dan Gilleland <dgilleland@nait.ca>
2021-01-21 08:57:39 -0700

    Refactored Die Game driver

--- src/demos/CSharp/FunAndGames/Sandbox/Die.cs
+++ src/demos/CSharp/FunAndGames/Sandbox/Die.cs
@@ -5,19 +5,19 @@ namespace Sandbox
     // The Die class will act like a template for creating objects.
     // Die objects are things that we can roll and that we can
     // see the "side" that is on the top of the die.
-    public class Die
+    public class Die // Every class you create is a new data type that you can use
     {
         // Field for the Random Number Generator
         // This will be the only other "static" item
         // in order to improve it's ability to be random
-        private static Random _rnd = new Random();
+        private static Random _rnd = new Random(); // Shared among all instances of this Die class
 
         // Property for the side that is facing up.
         // This is an auto-implemented property
         public int FaceValue { get; private set; }
 
         // Field to store the # of sides
-        private int _Sides;
+        private int _Sides; // fields are typically made private to "protect" the data
         // Property for the number of sides for a die
         public int Sides
         {
@@ -40,6 +40,7 @@ namespace Sandbox
         public Die(int numberOfSides)
         {
             Sides = numberOfSides; // store the value using the property
+            Roll();
         }
 
         // Methods
