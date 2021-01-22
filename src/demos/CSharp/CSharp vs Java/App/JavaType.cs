736df5737072607c2de8d8d24facddbeb49bcf65
Dan Gilleland <dgilleland@nait.ca>
2021-01-22 15:27:08 -0700

    Java vs. CSharp

---
+++ src/demos/CSharp/CSharp vs Java/App/JavaType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class JavaType
+    {
+        // Fields should probably be private
+        private int _Count; // the _ is typically used to denote a private field
+        public void Count(int value)
+        {
+            if (value < 0) throw new Exception(); // Yay, I guarded by variable so only good data comes in.
+            _Count = value;
+        } // SetCount(int value)
+        public int Count() { return _Count; }            // GetCount()
+    }
+    public class CSharpType
+    {
+        private int _Count;
+        public int Count
+        {
+            get { return _Count; }
+            set
+            {
+                if (value < 0) throw new Exception();
+                _Count = value;
+            }
+        }
+    }
+}
---
+++ src/demos/CSharp/CSharp vs Java/App/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try { DemoJava(); } catch { }
+            Console.ForegroundColor = ConsoleColor.Green;
+            try { DemoCSharp(); } catch { }
+            Console.ResetColor();
+        }
+        static void DemoJava()
+        {
+            JavaType first, second;
+            first = new JavaType();
+            second = new JavaType();
+            Console.WriteLine($"First is {first.Count()} and Second is {second.Count()}"); // see the value
+            SetCount(first, 42);
+            SetCount(second, -5);
+        }
+        static void SetCount(JavaType item, int count)
+        {
+            item.Count(count); // Change the value
+        }
+
+        static void DemoCSharp()
+        {
+            CSharpType first, second;
+            first = new CSharpType();
+            second = new CSharpType();
+            Console.WriteLine($"First is {first.Count} and Second is {second.Count}"); // see the value
+            SetCount(first, 42);
+            SetCount(second, -5);
+        }
+        static void SetCount(CSharpType item, int count)
+        {
+            item.Count = count; // Change the value
+        }
+
+    }
+}
