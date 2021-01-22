ebf4dc32ea95f4b28ec1f4acc546a77942f543ee
Dan Gilleland <dgilleland@nait.ca>
2021-01-22 13:15:22 -0700

    Database Console App

---
+++ src/demos/CSharp/Database Console App/MyDb/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Course> data;
+            List<Assignment> marks;
+            // The following using keyword is different from the using keyword at the top of this file.
+            using (var context = new A03Context()) // "Clean-up" after "using" this object
+            {
+                data = context.Courses.ToList();
+                marks = context.Assignments.ToList();
+            }
+            foreach(Course course in data)
+            {
+                Console.WriteLine($"{course.CourseId} - {course.Name}");
+            }
+            foreach (Assignment item in marks)
+            {
+                Console.WriteLine($"{item.CourseId} - ({item.Weight} %) {item.Name}");
+            }
+        }
+    }
+    /// <summary>
+    /// This is an Entity class that mirrors the structure of the Courses table.
+    /// </summary>
+    public class Course
+    {
+        public string CourseId { get; set; }
+        public string Name { get; set; }
+    }
+    /// <summary>
+    /// This is an Entity clas that mirrors the structure of the Assignments table.
+    /// </summary>
+    public class Assignment
+    {
+        public int AssignmentId { get; set; }
+        public string Name { get; set; }
+        public short Weight { get; set; }
+        public string CourseId { get; set; }
+        public double? PossibleMarks { get; set; }
+        public double? EarnedMarks { get; set; }
+    }
+
+    /// <summary>
+    /// This is a virtual representation of my database.
+    /// </summary>
+    /// <seealso cref="System.Data.Entity.DbContext" />
+    public class A03Context : DbContext
+    {
+        public A03Context() : base("name=A03Db") // Identify the name for a connection string in .config
+        {
+            Database.SetInitializer<A03Context>(null); // Ensures that EF6 does not auto-generate the db
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+    }
+}
