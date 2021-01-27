ce463302d7459c4b4f6731f80282fbab687a263a
Dan Gilleland <dgilleland@nait.ca>
2021-01-27 11:48:26 -0700

    Improve understanding with more comments

--- src/w03/practice/js/main.js
+++ src/w03/practice/js/main.js
@@ -3,12 +3,15 @@
 var featureLink = document.querySelector('a.feature.link'); // Step 1
 
 // Another common term for our event-handling functions is "callback"
+// The following lines do two things: 1) creates a constant, 2) declares/defines a function with instructions
+// Using a const variable to refer to our function protects it from being changed by other
 const featureLinkHandler = function (evt) { // Step 2
     let featureImage = document.querySelector('img.feature');
     featureImage.src = evt.target.href;
     featureImage.classList.remove('hidden');
     evt.preventDefault(); // Last Step - stopping the default behaviour of anchor tag 
 }
+
 
 featureLink.addEventListener('click', featureLinkHandler); // Step 3
 //                                   \callback function/
