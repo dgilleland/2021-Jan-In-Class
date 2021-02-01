7b75a58bb8f7a806a74f8258d3234e768a66153a
Dan Gilleland <dgilleland@nait.ca>
2021-02-01 05:35:32 -0700

    Basic Intro to Razor

---
+++ src/demos/Razor/Basic Intro/WebApp/BeginHere/UIControlDemos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.BeginHere
+{
+    public enum UIControlDemos
+    {
+        INTRO,
+        CHECKBOX,
+        PASSWORD,
+        RADIO,
+        TEXT,
+        SELECT
+    }
+}
