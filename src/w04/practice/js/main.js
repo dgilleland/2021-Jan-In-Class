84bd94fd85ae2bca0c85e742881f946bd25bd563
Dan Gilleland <dgilleland@nait.ca>
2021-02-02 10:30:27 -0700

    Refactor starting code in practice exercise

--- src/w04/practice/js/main.js
+++ src/w04/practice/js/main.js
@@ -1,11 +1,18 @@
-var featureLink = document.querySelector('a.feature');
+// Refactoring the code to make it "better"
+// 1) Turned my function "variable" into a constant
+// 2) Improve the "readability" of my code by choosing better names
+// 3) Use let instead of var
+// 4) Use local variables in functions where available =>> "outside" variable vs. local variable
+//    Benefit of "isolation"
+// 5) Limit the use of "temporary" global variables =>> "inline" the variable featureLink
 
-function featureLinkHandler(evt) {
-    var featureImage = document.querySelector('img.feature');
-    featureImage.src = featureLink.href;
+const toggleImage = function(evt) {
+    let featureImage = document.querySelector('img.feature');
+    featureImage.src = evt.target.href;
     featureImage.classList.remove('hidden');
 
     evt.preventDefault();
 }
 
-featureLink.addEventListener('click', featureLinkHandler);
+document.querySelector('a.feature') // formerly as the variable     featureLink
+        .addEventListener('click', toggleImage);
