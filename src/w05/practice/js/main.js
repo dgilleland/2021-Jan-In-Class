d791de5a1af0f010f91410e088e224a144fe9fd6
Dan Gilleland <dgilleland@nait.ca>
2021-02-03 11:25:31 -0700

    practice solution

--- src/w05/practice/js/main.js
+++ src/w05/practice/js/main.js
@@ -4,3 +4,24 @@
 // (i.e. cannot be whitespace). If the user tries to add an empty tag, remove the hidden 
 // class from the p.feature.error element.  If the form is submitted correctly, then ensure
 // that the p.feature.error element is hidden.
+
+document.querySelector('.feature.frm ')
+.addEventListener('submit', function (evt) {
+    let frm = evt.target;
+    let tag = frm.elements.tags;
+    var error = document.querySelector('p.feature.error');
+
+    // Ensure that there is a value in the tag input field before adding the tag to the paragraph
+    if (tag.value.trim() != '') {
+        // insert a '# ' before the tag for aesthetics
+        document.querySelector('p.feature.tags ').innerHTML += '#' + tag.value.trim() + ' ';
+        tag.value = '';
+        error.classList.add('hidden');
+    } else {
+        // No data was entered
+        error.innerHTML = "Blank input will not be processed.";
+        error.classList.remove('hidden');
+    }
+    evt.preventDefault(); 
+});
+
