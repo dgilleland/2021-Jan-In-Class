861ff21c989ab55fae05be9abf3b6a0083e7d005
Dan Gilleland <dgilleland@nait.ca>
2021-02-09 09:55:29 -0700

    Search Page + missing Entity

---
+++ src/demos/Razor/WestWind/WebApp/Data/EmployeeTerritory.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Data
+{
+    // ![](BE0B1BF22D77A0551161BF09E219B433.png)
+    public class EmployeeTerritory
+    {
+        // I have a composite Primary Key in this table. I can identify it with attributes
+        [Key, Column(Order = 1)]
+        public int EmployeeID { get; set; }
+        [Key, Column(Order = 2)]
+        public int TerritoryID { get; set; }
+    }
+}
