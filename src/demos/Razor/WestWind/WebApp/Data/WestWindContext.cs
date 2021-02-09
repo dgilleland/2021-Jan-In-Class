072ae5997c1c04551793aa2ba69f6b4c216987c2
Dan Gilleland <dgilleland@nait.ca>
2021-02-09 09:54:57 -0700

    Database Context class

---
+++ src/demos/Razor/WestWind/WebApp/Data/WestWindContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity; // Contains our DbContext class
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Data
+{
+    public class WestWindContext : DbContext
+    {
+        public WestWindContext() : base("name=WWdb")
+        {
+            Database.SetInitializer<WestWindContext>(null); // Don't try re-creating any db tables
+        }
+
+        // Declare a property for each table I want to access
+        public DbSet<BuildVersion> BuildVersions { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        // TODO: Homework! - Add properties for all the remaining tables/entities in your database/Entity classes
+    }
+}
