31cb2029d919a0074a50af5263e800ec35fa227b
Dan Gilleland <dgilleland@nait.ca>
2021-02-22 18:55:35 -0700

    Demonstrate TryParse

--- src/demos/CSharp/Learning TryParse/MyApp/Program.cs
+++ src/demos/CSharp/Learning TryParse/MyApp/Program.cs
@@ -29,25 +29,67 @@ namespace MyApp
         private void AnalyzeInput(string input)
         {
             // Test to see if it is a whole number
-            try
-            {
-                int wholeNumber = int.Parse(input);
+            int wholeNumber;
+            if (int.TryParse(input, out wholeNumber))
                 WriteLine($"{wholeNumber} is an integer.");
-            }
-            catch (Exception ex)
-            {
+            else
                 WriteLine("It is not a whole number.");
-            }
 
-            // Test to see if it a real number
+            // Test to see if it is a real number
+            double realNumber;
+            if( double.TryParse(input, out realNumber))
+                WriteLine($"{realNumber} is a real number.");
+            else
+                WriteLine("It is not a real number.");
+
+            // Test to see if it is a fraction
+            Fraction frac;
+            if (Fraction.TryParse(input, out frac))
+                WriteLine($"{frac} is a fraction with a double equivalent of {frac.ToDouble()}");
+            else
+                WriteLine("It is not a fraction");
+        }
+    }
+
+    public class Fraction
+    {
+        public int Numerator { get; }
+        public int Denominator { get; }
+        public Fraction(int num, int denom)
+        {
+            Numerator = num;
+            Denominator = denom;
+        }
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public double ToDouble()
+        {
+            return (double)Numerator / Denominator;
+        }
+
+        public static Fraction Parse(string input)
+        {
+            // Simple parsing of a string - we just let any errors get thrown
+            var parts = input.Split('/');
+            return new Fraction(int.Parse(parts[0]), int.Parse(parts[1]));
+        }
+
+        public static bool TryParse(string input, out Fraction result)
+        {
+            // Since the result parameter is an out parameter, I have to ensure its value is set whether
+            // I succeed or not
             try
             {
-                double realNumber = double.Parse(input);
-                WriteLine($"{realNumber} is a real number.");
+                result = Parse(input); // Happy path
+                return true;
             }
-            catch // Identifying the exception is optional if you are not going to use it. (but you SHOULD USE IT)
+            catch
             {
-                WriteLine("It is not a real number.");
+                result = null; // Failed. Give a null object as the default
+                return false;
             }
         }
     }
