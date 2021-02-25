b90b227e8b298d47e93398e50a989c0d5adc49c8
Dan Gilleland <dgilleland@nait.ca>
2021-02-25 08:51:36 -0700

    Handling null values when displaying.

--- src/demos/Razor/WestWind/WebApp/Data/Order.cs
+++ src/demos/Razor/WestWind/WebApp/Data/Order.cs
@@ -16,7 +16,7 @@ namespace WebApp.Data
         public int OrderID { get; set; }
         public int? SalesRepID { get; set; }
         public string CustomerID { get; set; }
-        public DateTime? OrderDate { get; set; }
+        public DateTime? OrderDate { get; set; } // Nullable<DateTime>
         public DateTime? RequiredDate { get; set; }
         public DateTime? PaymentDueDate { get; set; }
         public decimal? Freight { get; set; }
