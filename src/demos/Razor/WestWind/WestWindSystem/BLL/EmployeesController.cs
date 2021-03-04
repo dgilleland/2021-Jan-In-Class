f12cfd76e915bc1503bdda7ccddae2fda535e5fd
Dan Gilleland <dgilleland@nait.ca>
2021-03-04 09:25:19 -0700

    Refactoring: EmployeesController

---
+++ src/demos/Razor/WestWind/WestWindSystem/BLL/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Data;
+
+namespace WestWindSystem.BLL
+{
+    public class EmployeesController
+    {
+        public List<Employee> ListEmployees()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Employees.ToList();
+            }
+        }
+    }
+}
