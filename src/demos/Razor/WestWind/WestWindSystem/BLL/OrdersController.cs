6bbdc5517af0da188c36491a3a3447b6707504a0
Dan Gilleland <dgilleland@nait.ca>
2021-03-04 09:00:09 -0700

    Refactoring: GetOrder, AddOrder, UpdateOrder

--- src/demos/Razor/WestWind/WestWindSystem/BLL/OrdersController.cs
+++ src/demos/Razor/WestWind/WestWindSystem/BLL/OrdersController.cs
@@ -7,6 +7,9 @@ using WebApp.Data;
 
 namespace WestWindSystem.BLL
 {
+    /// <summary>
+    /// Handles all of the CRUD behaviour for interacting with the Orders table.
+    /// </summary>
     public class OrdersController
     {
         public List<Order> GetCustomerOrders(string customerId)
@@ -19,6 +22,40 @@ namespace WestWindSystem.BLL
                              orderby purchase.OrderDate descending // Most recent purchases first
                              select purchase;
                 return result.ToList();
+            }
+        }
+
+        public Order GetOrder(int orderId)
+        {
+            using (var context = new WestWindContext())
+            {
+                Order existing = context.Orders.Find(orderId);
+                return existing;
+            }
+        }
+
+        public int AddOrder(Order newOrder)
+        {
+            using (var context = new WestWindContext())
+            {
+                // 2) Add this to the "virtual" database (our DbContext object)
+                context.Orders.Add(newOrder);
+                // 3) Saving the changes to the "actual" database
+                context.SaveChanges(); // This is when the actual SQL INSERT occurs
+
+                return newOrder.OrderID;
+            }
+        }
+
+        public void UpdateOrder(Order updated)
+        {
+            using (var context = new WestWindContext())
+            {
+                // 2) Note the changes to the order data
+                var existing = context.Entry(updated);  // Associate the updatedOrder with an existing order
+                existing.State = System.Data.Entity.EntityState.Modified; // Note that the data for this order has changed
+                                                                          // 3) Save the changes to the actual database
+                context.SaveChanges();
             }
         }
     }
