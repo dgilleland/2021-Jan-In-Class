afb0662a1a4d01c9010f09fb164d89567bed11b2
Dan Gilleland <dgilleland@nait.ca>
2021-03-04 08:26:55 -0700

    Refactor getting customer orders

---
+++ src/demos/Razor/WestWind/WestWindSystem/BLL/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Data;
+
+namespace WestWindSystem.BLL
+{
+    public class OrdersController
+    {
+        public List<Order> GetCustomerOrders(string customerId)
+        {
+            using (var context = new WestWindContext())
+            {
+                // I'll use a C# LINQ statement to query the database for the orders belong to a specific customer
+                var result = from purchase in context.Orders
+                             where purchase.CustomerID == customerId
+                             orderby purchase.OrderDate descending // Most recent purchases first
+                             select purchase;
+                return result.ToList();
+            }
+        }
+    }
+}
