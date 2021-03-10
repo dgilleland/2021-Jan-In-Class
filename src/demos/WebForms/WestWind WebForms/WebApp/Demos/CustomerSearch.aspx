8ebe3b7414511d39b19f898f4ae5f351a301f097
Dan Gilleland <dgilleland@nait.ca>
2021-03-10 16:51:50 -0700

    Demo Web Forms in Customer Search.

---
+++ src/demos/WebForms/WestWind WebForms/WebApp/Demos/CustomerSearch.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CustomerSearch.aspx.cs" Inherits="WebApp.Demos.CustomerSearch" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1 class="page-header">Customer Search</h1>
+
+    <div class="row">
+        <div class="col">
+            <div class="form-inline">
+                <asp:TextBox ID="PartialName" runat="server" CssClass="form-control" placeholder="Partial Customer Name" />
+                &nbsp;
+                <asp:LinkButton ID="FindCustomers" runat="server" CssClass="btn btn-md btn-secondary"
+                    OnClick="FindCustomers_Click">
+                    Find Customer
+                </asp:LinkButton>
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-inline">
+                <asp:Label ID="Label1" runat="server" AssociatedControlID="MatchingNames">Matching Names: </asp:Label>
+                <asp:DropDownList ID="MatchingNames" runat="server" CssClass="form-control"></asp:DropDownList>
+                &nbsp;
+                <asp:LinkButton ID="ShowOrders" runat="server" CssClass="btn btn-md btn-secondary"
+                    OnClick="ShowOrders_Click">
+                    Show Orders
+                </asp:LinkButton>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <asp:Label ID="MessageLabel" runat="server" />
+        </div>
+    </div>
+</asp:Content>
---
+++ src/demos/WebForms/WestWind WebForms/WebApp/Demos/CustomerSearch.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WestWindSystem.BLL;
+using WestWindSystem.Entities;
+
+namespace WebApp.Demos
+{
+    public partial class CustomerSearch : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack) // GET Request
+            {
+                // Populate my drop-down with default data
+                MatchingNames.Items.Add(new ListItem("[Select a customer]", string.Empty));
+            }
+            MessageLabel.Text = "";
+        }
+
+        protected void FindCustomers_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(PartialName.Text))
+            {
+                MessageLabel.Text = "Enter at least one character for the partial name.";
+            }
+            else
+            {
+                // Populate the Matching Names dropdown with data.
+                // 1) Get information from the BLL
+                var controller = new CustomerController();
+                var customers = controller.LoadCustomers(PartialName.Text);
+
+                // 2) Display the information in the drop-down control
+                MatchingNames.DataSource = customers; // Telling the control what data to use
+                MatchingNames.DataTextField = nameof(Customer.CompanyName); // Text to display for each item
+                MatchingNames.DataValueField = nameof(Customer.CustomerID); // Value to use (PK of the customer)
+                MatchingNames.DataBind(); // go ahead and extract the appropriate info from .DataSource to generate your HTML
+                MatchingNames.Items.Insert(0, new ListItem("[Select a customer]", string.Empty));
+            }
+        }
+
+        protected void ShowOrders_Click(object sender, EventArgs e)
+        {
+            // 0) Validation for ShowOrders
+            if (MatchingNames.SelectedIndex == 0) // The first item, which is a "prompt" inside the <select> element
+            {
+                MessageLabel.Text = "You must select a customer to view their orders.";
+            }
+            else
+            {
+                // Navigate to the other page
+                var customerId = MatchingNames.SelectedValue; // Grabbing the value for the selected item in the drop-down
+                Response.Redirect($"~/Demos/ViewCustomerOrders.aspx?customerId={customerId}", true);
+            }
+        }
+    }
+}
