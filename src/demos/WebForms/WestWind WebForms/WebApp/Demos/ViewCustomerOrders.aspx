2d8270e4c38f208c1e4836f3570c1aa97357a4ca
Dan Gilleland <dgilleland@nait.ca>
2021-03-15 14:54:22 -0600

    View Customer Orders

--- src/demos/WebForms/WestWind WebForms/WebApp/Demos/ViewCustomerOrders.aspx
+++ src/demos/WebForms/WestWind WebForms/WebApp/Demos/ViewCustomerOrders.aspx
@@ -2,4 +2,34 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <h1 class="page-header">View Customer Orders</h1>
+
+    <div class="row">
+        <div class="col-md-3">
+            <!-- Summary information about the customer -->
+            <h2><asp:Label ID="CustomerName" runat="server" /></h2>
+            <asp:Label ID="ContactName" runat="server" />
+            -
+            <asp:Label ID="PhoneNumber" runat="server" />
+            <br />
+            <asp:LinkButton ID="AddOrder" runat="server" CssClass="btn btn-secondary">Add Order</asp:LinkButton>
+        </div>
+        <div class="col-md-9">
+            <!-- HTML Table for all of the orders -->
+            <h2>Orders</h2>
+
+            <asp:GridView ID="OrdersGridView" runat="server" CssClass="table table-hover" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton ID="EditLink" runat="server" CssClass="btn btn-secondary btn-sm">Edit</asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="OrderDate" DataFormatString="{0:yyyy-MM-dd}" HeaderText="Order Date" />
+                    <asp:BoundField DataField="ShipName" HeaderText="Ship Name" />
+                    <asp:CheckBoxField DataField="Shipped" HeaderText="Shipped" />
+                    <asp:BoundField DataField="Freight" HeaderText="Freight" DataFormatString="{0:C}" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
 </asp:Content>
--- src/demos/WebForms/WestWind WebForms/WebApp/Demos/ViewCustomerOrders.aspx.cs
+++ src/demos/WebForms/WestWind WebForms/WebApp/Demos/ViewCustomerOrders.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WestWindSystem.BLL;
 
 namespace WebApp.Demos
 {
@@ -11,7 +12,34 @@ namespace WebApp.Demos
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack) // GET Request
+            {
+                // Retreive the customer and customer order information for the form
+                // 1) Manually grab the querystring data
+                string customerId = Request.QueryString[nameof(customerId)];
 
+                // 2) Lookup the specfic customer details
+                CustomerController controller = new CustomerController();
+                var details = controller.FindCustomer(customerId);
+
+                // 2a) Do a re-direct if we don't have this customer
+                if (details == null)
+                    Response.Redirect("~/Demos/CustomerSearch", true);
+
+                // 3) Display those details in my form
+                CustomerName.Text = details.CompanyName;
+                ContactName.Text = details.ContactName;
+                PhoneNumber.Text = details.Phone;
+
+                // 4) Lookup the orders for this customer
+                OrdersController ordController = new OrdersController();
+                var orders = ordController.GetCustomerOrders(customerId);
+
+                // 5) Display the orders in the GridView
+                //    The GridView is a DataBound Control. Other DataBound controls include DropDownList, ListView, Repeater.
+                OrdersGridView.DataSource = orders;
+                OrdersGridView.DataBind();
+            }
         }
     }
 }
