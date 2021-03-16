e92774e0cb99103ce6feb37374863914c7b2f563
Dan Gilleland <dgilleland@nait.ca>
2021-03-16 15:45:02 -0600

    Stopwatch calculation

--- src/w10/demos/StopWatch/main.js
+++ src/w10/demos/StopWatch/main.js
@@ -6,3 +6,10 @@ function displayTime() {
 
   displayTime();
   const createClock = setInterval(displayTime, 1000);
+
+const calcTimer = function (total) {
+    let hours = Math.floor(total / 3600);
+    let minutes = Math.floor((total - hours * 3600) / 60);
+    let seconds = total % 60;
+    return {hours, minutes, seconds};
+}
