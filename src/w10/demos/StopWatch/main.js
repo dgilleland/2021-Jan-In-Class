9264a48fcd737fcbe98ecdb5f52bbbf75eb2a895
Dan Gilleland <dgilleland@nait.ca>
2021-03-17 13:44:30 -0600

    Todo list in Stopwatch practice

--- src/w10/demos/StopWatch/main.js
+++ src/w10/demos/StopWatch/main.js
@@ -1,3 +1,9 @@
+// TODO:
+// A "Start" button to start the stopwatch running.
+// A "Stop" button to pause/stop it.
+// A "Reset" button to reset the time back to 0.
+// The time display to show the number of seconds elapsed, rather than the actual time.
+
 function displayTime() {
     let date = new Date();
     let time = date.toLocaleTimeString();
