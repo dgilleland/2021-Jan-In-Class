6b64d32007519795d370716a5fdd7c0169487f8e
Dan Gilleland <dgilleland@nait.ca>
2021-03-17 16:49:00 -0600

    Making Strings Quack - Extension Methods

---
+++ src/demos/CSharp/ExtensionMethodDemo/App/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello World!";
+
+            Console.WriteLine(greeting);
+            Console.WriteLine(greeting.Length);
+            Console.WriteLine(greeting.Quack());
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static string Quack(this string self)
+        {
+            return $"{self} (quack)";
+        }
+    }
+}
