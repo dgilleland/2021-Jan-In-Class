967c8852ed19cb671cfaa4f7affe4c9ba7fbb640
Dan Gilleland <dgilleland@nait.ca>
2021-03-25 09:26:35 -0600

    Customizing GridView + RadioButtonList + other odds-n-ends

---
+++ src/demos/WebForms/Odds-n-Ends/MyBackendServices/BLL/FakeData.cs
@@ -0,0 +1,66 @@
+﻿using MyBackendServices.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBackendServices.BLL
+{
+    [DataObject]
+    public class FakeData
+    {
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<CourseMarks> ListCourseMarks()
+        {
+            List<CourseMarks> result = new List<CourseMarks>();
+
+            // TODO: Add some data
+            result.Add(new CourseMarks { Number = "PROG-0101", Name = "Programming Fundamentals", ProgramId = 1, Credits = 4.5, FinalMark = 80 });
+            result.Add(new CourseMarks { Number = "PROG-1508", Name = "Practical Database Fundamentals", ProgramId = 1, Credits = 4.5, FinalMark = 72 });
+            result.Add(new CourseMarks { Number = "PROG-1022", Name = "Intro to Systems Analysis", ProgramId = 2, Credits = 4.5, FinalMark = 64 });
+            result.Add(new CourseMarks { Number = "HACK-0101", Name = "Hacking Fundamentals", ProgramId = 3, Credits = 4.5, FinalMark = 71 });
+            result.Add(new CourseMarks { Number = "HACK-1212", Name = "Shell Access Control", ProgramId = 3, Credits = 4.5, FinalMark = 68 });
+            result.Add(new CourseMarks { Number = "HACK-0100", Name = "White Hat/Black Hat History", ProgramId = 3, Credits = 4.5, FinalMark = 0 });
+            //result.Add(new CourseMarks { Number = "", Name = "", Credits = 4.5, FinalMark = 0 });
+
+            return result;
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<StudyProgram> ListStudyPrograms()
+        {
+            List<StudyProgram> result = new List<StudyProgram>();
+
+            result.Add(new StudyProgram { ProgramId = 1, Name = "Software Development" });
+            result.Add(new StudyProgram { ProgramId = 2, Name = "Analysis" });
+            result.Add(new StudyProgram { ProgramId = 3, Name = "Cyberdefense" });
+
+            return result;
+        }
+
+        public List<Fish> ListFishThings()
+        {
+            using(var context = new DAL.AquamanContext())
+            {
+                return context.SlimeyThings.ToList();
+            }
+        }
+    }
+
+    public class CourseMarks
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public double Credits { get; set; }
+        public int FinalMark { get; set; }
+        public int ProgramId { get; set; }
+    }
+
+    public class StudyProgram
+    {
+        public int ProgramId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/demos/WebForms/Odds-n-Ends/WebApp/Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using MyBackendServices.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                var controller = new FakeData();
+                var data = controller.ListFishThings();
+                FishGridView.DataSource = data;
+                FishGridView.DataBind();
+            }
+        }
+
+        protected void AdHocGridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string theCourse = AdHocGridView.SelectedValue.ToString(); // This is getting the Number of the course
+            string citizenship = CitizenshipList.SelectedValue;
+            string studyMajor = StudyProgramList.SelectedValue;
+
+            string alertStyle = "alert ";
+            string msg;
+            if(string.IsNullOrEmpty(citizenship) || string.IsNullOrEmpty(studyMajor))
+            {
+                msg = "You should specify your citizenship and study major to get your credit redemption.";
+                alertStyle += "alert-warning";
+            }
+            else
+            {
+                msg = $"You credit redemption for course {theCourse} will be processed within 5 business days. If you are successful, then you will be contacted by a CSIS agent to be interviewed as part of our Cyber-Security Task Force.";
+                if(!bool.Parse(citizenship))
+                {
+                    msg += "<br />Preference is given to Canadian Citizens.";
+                }
+                alertStyle += "alert-success";
+            }
+
+            MessageLabel.Text = msg;
+            MessageContainer.Attributes.Add("class", alertStyle);
+        }
+    }
+}
