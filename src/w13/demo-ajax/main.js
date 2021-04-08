40d77f835fdc296293ce7200699ff454f7c83650
Dan Gilleland <dgilleland@nait.ca>
2021-04-08 12:17:51 -0600

    Demo <template> and JSON data

--- src/w13/demo-ajax/main.js
+++ src/w13/demo-ajax/main.js
@@ -11,6 +11,36 @@ const loadMarks = async function() {
 // the console window
 const dumpData = function(data) {
     console.log('results: ', data);
+
+    // Using DOM API to create document fragements for the student name
+    let h2Tag = document.createElement('h2');
+    let text = document.createTextNode(data.Student);
+    let header = document.querySelector('header');
+    h2Tag.appendChild(text);
+    header.appendChild(h2Tag);
+
+    // Displaying all the marks in the <tbody> of my table
+    let tbody = document.getElementById('evaluation-items');
+    let totalWeight = 0, totalEarned = 0; // some totals
+    for(let mark of data.Marks){
+        // running calculations
+        totalWeight += mark.Weight;
+        if(mark.Earned) totalEarned += mark.Earned;
+        // Use my <template>'s content as a ready-built document fragment
+        let template = document.getElementById('item-row'); // get the template
+        let clone = template.content.cloneNode(true); // create a clone of the template content
+        // Place my JSON data into the cloned document fragment
+        clone.querySelector('[data-id=Name').innerText = mark.Name;
+        clone.querySelector('[data-id=Weight').innerText = mark.Weight;
+        clone.querySelector('[data-id=Earned').innerText = mark.Earned || '--';
+        tbody.appendChild(clone);
+    }
+    document.getElementById('total-weight').innerText = totalWeight;
+    document.getElementById('total-earned').innerText = totalEarned;
+
+    // Loop through the rows
+    // Add the row to the table
+
 }
 
 
